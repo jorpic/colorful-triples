66ad4c4c5150a13b488ea2a40e8f386ab93ffd47
Max Taldykin <jorpic@gmail.com>
2023-05-19 12:18:47 +0400

    experiment1 & experiment2

--- src/main.rs
+++ src/main.rs
@@ -9,44 +9,35 @@ mod brute_force;
 use brute_force::*;
 
 
+const N: u64 = 7825;
 
 fn main() {
-    const N: u64 = 7825;
+    experiment1();
+    experiment2();
+}
+
+fn get_neighbors(
+    links: &LinksMap,
+    depth: usize, weight: usize, link_size: usize)
+    -> BTreeSet<Triples>
+{
+    let mut blocks = BTreeSet::new();
+    for (l, _) in links.iter() {
+        let b = links.neighbours(*l, depth).filter_by_link_weight(weight);
+        if b.link_set().len() == link_size {
+            blocks.insert(b);
+        }
+    }
+    println!("{} blocks of width 2-2-27 found", blocks.len());
+    blocks
+}
+
+fn experiment1() {
     let py = Triples::pythagorean(N).filter_by_link_weight(2);
     let links = py.links_map();
-    assert_eq!(py.len(), 7336);
-    assert_eq!(links.len(), 3745);
 
-    let mut blocks_2 = BTreeSet::new();
-    for (l, _) in links.iter() {
-        let b = links.neighbours(*l, 2).filter_by_link_weight(2);
-        if b.link_set().len() == 27 {
-            blocks_2.insert(b);
-        }
-    }
-    println!("{} blocks of width 2-2-27 found", blocks_2.len());
-
-    for b in blocks_2.iter().take(2) {
-        let xxx = BfBlock::new(&b);
-        println!("{}", xxx);
-        xxx.pair_sets();
-    }
-
-    let mut blocks_3 = BTreeSet::new();
-    for (l, _) in links.iter() {
-        let b = links.neighbours(*l, 3).filter_by_link_weight(3);
-        if b.link_set().len() == 27 {
-            blocks_3.insert(b);
-        }
-    }
-    println!("{} blocks of width 3-3-27 found", blocks_3.len());
-
-    for b in blocks_3.iter().take(2) {
-        let xxx = BfBlock::new(&b);
-        println!("{}", xxx);
-        xxx.pair_sets();
-    }
-
+    let blocks_2 = get_neighbors(&links, 2, 2, 27);
+    let blocks_3 = get_neighbors(&links, 3, 3, 27);
     let mut blocks: Vec<_> = blocks_2.union(&blocks_3)
         .map(|x| BfBlock::new(&x))
         .collect();
@@ -68,5 +59,24 @@ fn main() {
                 }
             }
         }
+    }
+}
+
+fn experiment2() {
+    let py = Triples::pythagorean(N).filter_by_link_weight(2);
+    let links = py.links_map();
+
+    let blocks_2 = get_neighbors(&links, 2, 2, 27);
+    for b in blocks_2.iter().take(2) {
+        let xxx = BfBlock::new(&b);
+        println!("{}", xxx);
+        xxx.pair_sets();
+    }
+
+    let blocks_3 = get_neighbors(&links, 3, 3, 27);
+    for b in blocks_3.iter().take(2) {
+        let xxx = BfBlock::new(&b);
+        println!("{}", xxx);
+        xxx.pair_sets();
     }
 }
