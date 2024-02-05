afc1479067f65fd3540cf7f900064f22f08bb7d8
Max Taldykin <jorpic@gmail.com>
2024-02-05 13:06:01 +0400

    make more semi-tight subgraphs

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,87 @@
+use std::collections::BTreeSet;
+
 mod triples;
 use triples::*;
 
 mod brute_force;
 use brute_force::*;
 
-
 fn main() {
     let triples = pythagorean_triples(7825);
+
     let mut subs = all_subgraphs(&triples, 3)
         .iter()
         .map(|s| drop_weak_links(s, 3))
         .filter(|s| s.len() > 0)
+        .map(|s| (links(&s).len(), s.len(), s))
         .collect::<Vec<_>>();
-
     subs.sort();
     subs.dedup();
-    let mut subs = subs.iter()
-        .map(|s| (s, links(s)))
-        .collect::<Vec<_>>();
-    subs.sort_unstable_by(
-        |(_, a),(_, b)| a.len().cmp(&b.len())
-    );
-    println!("{:?}", subs.len());
 
-    for i in 0..100 {
-        println!(
-            "{}: has {} triples, {} links and {} solutions",
-            i,
-            subs[i].0.len(),
-            subs[i].1.len(),
-            brute_force(&subs[i].0)
-        );
+    println!("3,3 tight subgraphs: {}", subs.len());
+
+    let s27 = subs
+        .iter()
+        .filter(|s| 20 < s.0 && s.0 <= 32)
+        .collect::<Vec<_>>();
+
+    println!("subgraphs in s27: {}", s27.len());
+
+    let mut used_triples: BTreeSet<Triple> = BTreeSet::new();
+    for s in &s27 {
+        for t in &s.2 {
+            used_triples.insert(*t);
+        }
+    }
+
+    println!("unique triples in s27: {}", used_triples.len());
+
+    let subs = all_subgraphs(&triples, 3)
+        .iter()
+        .map(|s| drop_weak_links(s, 2))
+        .filter(|s| s.len() > 0)
+        .map(|s| (links(&s).len(), s.len(), s))
+        .collect::<Vec<_>>();
+    // subs.sort();
+    // subs.dedup(); // we don't need this because triples are used only once
+
+    println!("4,2 tight subgraphs: {}", subs.len());
+
+    let mut res = Vec::new();
+    for (_lns, _tps, sub) in subs {
+        let new_sub = sub
+            .iter()
+            .filter(|t| !used_triples.contains(*t))
+            .cloned()
+            .collect::<Vec<Triple>>();
+
+        let new_lns = links(&new_sub).len();
+        let new_tps = new_sub.len();
+
+        if new_tps < 6 || new_lns < 20 || new_lns > 32 {
+            continue;
+        }
+
+        if new_lns - new_tps <= new_tps {
+            // println!("\t>> {} links, {} triples", new_lns, new_tps);
+            for t in &new_sub {
+                used_triples.insert(*t);
+            }
+            res.push((new_lns, new_tps, new_sub));
+        }
+    }
+
+    println!(
+        "with {} extra subgraphs we cover {} unique triples",
+        res.len(),
+        used_triples.len()
+    );
+
+    for s in res {
+        print!("lns={}, tps={}", s.0, s.1);
+        println!(", sol={:>10}", brute_force(&s.2));
     }
 }
-
 
 fn to_string(triples: &[Triple]) -> String {
     let mut res = String::new();
