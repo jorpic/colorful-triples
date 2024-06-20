b64773fcffc985cd79c81e7add823eff72927f2d
Max Taldykin <jorpic@gmail.com>
2024-06-21 00:06:28 +0400

    cargo clippy

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,15 @@ fn main() -> anyhow::Result<()> {
     let hgraph = drop_weak_nodes(&hgraph, 2);
     let hgraph = join_weak_nodes(&hgraph, 3, 42);
 
-    let edge_weights: BTreeMap<_, _> = mk_edge_index(&hgraph)
-        .into_iter()
-        .map(|(edge, nodes)| (edge, nodes.len()))
-        .collect();
+    let global_edge_ix: BTreeMap<_, _> = mk_edge_index(&hgraph);
 
-    serde_json::to_writer(std::io::stdout(), &edge_weights)?;
+    let nhs = tight_neighborhoods(&global_edge_ix, 3, 3);
+
+    for n in nhs {
+        println!("triples={} edges={}", n.triples().count(), n.edges().count());
+    }
+
+    // serde_json::to_writer(std::io::stdout(), &nhs)?;
     // serde_json::to_writer(std::io::stdout(), &step1)?;
 
     Ok(())
