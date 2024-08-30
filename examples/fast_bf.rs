66e10dedaf32a3ab09839de0e2f5f26cc9c29559
Max Taldykin <jorpic@gmail.com>
2024-08-30 19:26:22 +0400

    use SIMD in fast_bf

--- examples/fast_bf.rs
+++ examples/fast_bf.rs
@@ -1,4 +1,6 @@
+#![feature(portable_simd)]
 use std::collections::BTreeSet;
+use std::simd::{u64x4, Simd, cmp::SimdPartialEq};
 
 pub type Edge = u16;
 pub type Triple = [Edge; 3];
@@ -92,6 +94,73 @@ pub fn fast_brute_force(base: &[Triple], exts: &[Triple]) -> Vec<u64> {
     solutions
 }
 
+pub fn simd_fast_brute_force(base: &[Triple], exts: &[Triple]) -> Vec<u64> {
+    // base edges in order as they occur in base triples
+    let base_edges: Vec<Edge> = base
+        .iter().flatten().cloned().collect();
+
+    let edges: Vec<Edge> =
+        base_edges
+            .iter()
+            .chain(edges(exts).difference(&edges(base)))
+            .cloned()
+            .collect();
+
+    let masks: Vec<u64> = exts.into_iter()
+        .map(|t| {
+            let mut mask: u64 = 0;
+            for l in t {
+                let i = edges.iter().position(|e| e == l).unwrap();
+                mask |= 1 << i;
+            }
+            mask
+        })
+        .collect();
+
+    let vectorized_masks: Vec<u64x4> = masks
+        .chunks(u64x4::LEN)
+        .map(|chunk| Simd::load_or(chunk, u64x4::splat(chunk[0])))
+        .collect();
+    let zero_vec = u64x4::splat(0);
+    let unit_vec = u64x4::splat(1);
+
+    let n: u64 = 2_u64.pow(edges.len() as u32);
+    let mut x = 0;
+
+    // starting value is 0b..._001_001_001
+    for _ in base {
+        x = (x << 3) | 0b001;
+    }
+
+    let mut solutions = vec![];
+    while x < n {
+        let mut x_vec = u64x4::splat(x);
+        for _ in 0..6 {
+            let is_solution = vectorized_masks
+                .iter()
+                .all(|m_vec| {
+                    let mv_vec = m_vec & x_vec;
+                    (mv_vec.simd_ne(*m_vec) & mv_vec.simd_ne(zero_vec)).all()
+                });
+            if is_solution {
+                solutions.push(x_vec[0]);
+            }
+            x_vec += unit_vec;
+        }
+        x += 8;
+        let mut mask = 0b111_000;
+        let mut incr = 0b010_000;
+        while x & mask == mask {
+            x += incr;
+            mask = mask << 3;
+            incr = incr << 3;
+        }
+    }
+
+    solutions
+}
+
+
 pub fn main() {
     let cluster = [
         [324,432,540],    [324,2175,2199],  [432,2900,2932],  [540,1155,1275],
@@ -119,14 +188,14 @@ pub fn main() {
     }
 
     let now = std::time::Instant::now();
-    let res1 = fast_brute_force(&base, &exts);
+    let res1 = simd_fast_brute_force(&base, &exts);
     println!(
         "solutions: {}, time: {:?}",
         res1.len(),
         now.elapsed());
 
     let now = std::time::Instant::now();
-    let res2 = slow_brute_force(&base, &exts);
+    let res2 = fast_brute_force(&base, &exts);
     println!(
         "solutions: {}, time: {:?}",
         res2.len(),
@@ -141,4 +210,3 @@ pub fn main() {
         }
     }
 }
-
