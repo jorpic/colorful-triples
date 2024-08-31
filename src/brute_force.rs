2056f91480bb19e0846b360342e50dc436dedc51
Max Taldykin <jorpic@gmail.com>
2024-08-31 14:37:12 +0400

    move fast_bf from examples

--- src/brute_force.rs
+++ src/brute_force.rs
@@ -1,9 +1,15 @@
-use crate::hgraph::*;
 use std::collections::BTreeSet;
+use std::simd::{u64x4, Simd, cmp::SimdPartialEq};
 
-fn edges(triples: &[Triple]) -> BTreeSet<Edge> {
-    triples.iter().flatten().cloned().collect()
+use crate::hgraph::*;
+
+fn edges<'a, T>(triples: T) -> BTreeSet<Edge>
+    where
+        T: IntoIterator<Item = &'a Triple>,
+{
+    triples.into_iter().flatten().cloned().collect()
 }
+
 
 pub fn brute_force(triples: &[Triple], internal_edges: &BTreeSet<Edge>) -> usize {
     let mut edges: Vec<Edge> = edges(triples).into_iter().collect();
@@ -37,3 +43,73 @@ pub fn brute_force(triples: &[Triple], internal_edges: &BTreeSet<Edge>) -> usize
 
     solutions
 }
+
+pub fn fast_brute_force(c: &Cluster) -> usize {
+    let exts: Vec<Triple> = c.nodes.difference(&c.base)
+        .cloned().collect();
+
+    // base edges in order as they occur in base triples
+    let base_edges: Vec<Edge> = c.base
+        .iter().flatten().cloned().collect();
+
+    let edges: Vec<Edge> =
+        base_edges
+            .iter()
+            .chain(edges(&exts).difference(&edges(&c.base)))
+            .cloned()
+            .collect();
+
+    let masks: Vec<u64> = exts.into_iter()
+        .map(|t| {
+            let mut mask: u64 = 0;
+            for l in t {
+                let i = edges.iter().position(|e| *e == l).unwrap();
+                mask |= 1 << i;
+            }
+            mask
+        })
+        .collect();
+
+    let vectorized_masks: Vec<u64x4> = masks
+        .chunks(u64x4::LEN)
+        .map(|chunk| Simd::load_or(chunk, u64x4::splat(chunk[0])))
+        .collect();
+    let zero_vec = u64x4::splat(0);
+    let unit_vec = u64x4::splat(1);
+
+    let n: u64 = 2_u64.pow(edges.len() as u32 - 1);
+    let mut x = 0;
+
+    // starting value is 0b..._001_001_001
+    for _ in &c.base {
+        x = (x << 3) | 0b001;
+    }
+
+    let mut solutions = 0;
+    while x < n {
+        let mut x_vec = u64x4::splat(x);
+        for _ in 0..6 {
+            let is_solution = vectorized_masks
+                .iter()
+                .all(|m_vec| {
+                    let mv_vec = m_vec & x_vec;
+                    (mv_vec.simd_ne(*m_vec) & mv_vec.simd_ne(zero_vec)).all()
+                });
+            if is_solution {
+                solutions += 1; //.push(x_vec[0]);
+            }
+            x_vec += unit_vec;
+        }
+        x += 8;
+        let mut mask = 0b111_000;
+        let mut incr = 0b010_000;
+        while x & mask == mask {
+            x += incr;
+            mask = mask << 3;
+            incr = incr << 3;
+        }
+    }
+
+    solutions
+}
+
