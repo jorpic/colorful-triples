0ad8c92bd9e9d372581c0ecc377ca80faf36e95a
Max Taldykin <jorpic@gmail.com>
2024-10-18 23:10:24 +0400

    count chains

--- xx.hs
+++ xx.hs
@@ -1,8 +1,16 @@
+import Data.List (nub, sort)
+
 triple :: [[Int]]
 triple
   = filter (\xs -> xs /= [1,1,1] && xs /= [0,0,0])
   $ sequence
   $ replicate 3 [0,1]
+
+quad :: [[Int]]
+quad
+  = filter (\xs -> xs /= [1,1,0,0] && xs /= [0,0,1,1])
+  $ sequence
+  $ replicate 4 [0,1]
 
 pyramid1
   = filter (\xss -> map (!!0) xss /= [1,1,1] && map (!!0) xss /= [0,0,0])
@@ -15,17 +23,28 @@ pyramid2
   $ sequence
   $ replicate 3 pyramid1
 
-
 knot
   = filter (\[[a,b,c], [a1,d,e], [b1,f,g], [c1,h,x], [d1,f1,h1], [e1,g1,x1]] ->
     a == a1 && b == b1 && c == c1 && d == d1 && e == e1 && f == f1 && g == g1 && h == h1 && x == x1)
   $ sequence
   $ replicate 6 triple
 
--- 4 * pyramid1 = 688,747,536 (≈ 5Gb of 64bit words)
+chain_3_3_3 -- 30/32 [00100, 11011]
+  = nub $ sort
+  $ map (\[[a,b,x1], [x2,c,y1], [y2,d,e]] -> [a,b,c,d,e])
+  $ filter (\[[a,b,x1], [x2,c,y1], [y2,d,e]] -> x1 == x2 && y1 == y2)
+  $ sequence
+  $ replicate 3 triple
+
+chain_4_3 -- 30/32 [00100, 11011]
+  = nub $ sort
+  $ map (\[[a,b,c,d], [x,y,z]] -> [a,b,c,y,z])
+  $ filter (\[[a,b,c,d], [x,y,z]] -> d == x)
+  $ sequence
+  $ [quad, triple]
+
 fromBits :: [Int] -> Int
 fromBits = foldl (\r x -> 2*r + x) 0
-
 
 diff :: [Int] -> [Int]
 diff xs = zipWith subtract xs $ tail xs
